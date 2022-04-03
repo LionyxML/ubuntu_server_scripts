900cdb89725f3d2b32e40656f414cd88da662451
LionyxML <rahul.juliato@gmail.com>
2022-04-02 21:05:21 -0300

    feat: avoids full graphical install

--- minimumX.sh
+++ minimumX.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 
+clear
+echo "---------------------------------"
+echo "Installing for minimum X support!"
+echo "---------------------------------"
 echo ""
-echo "Installing for simple X support!"
-echo "--------------------------------"
 echo "Please run this script with sudo inside your home dir"
 echo ""
 echo "If you're not ready, Ctrl-C NOW!"
@@ -13,15 +15,16 @@ sleep 15
 
 apt-get update
 apt-get upgrade
-apt-get install xorg
 apt-get install xinit
 apt-get install x11-apps
 apt-get install openbox
 
-echo "exec xterm&" > .xinitrc
-echo "exec xclock&" >> .xinitrc
+echo "Creating .xinitrc..."
+echo "xterm&" > .xinitrc
+echo "xclock&" >> .xinitrc
 echo "exec openbox" >> .xinitrc
 
+echo "Creating .Xdefaults..."
 echo "XTerm*faceSize: 16" > .Xdefaults
 echo "XTerm*colorMode: on" >> .Xdefaults
 echo "XTerm*dynamicColors: on" >> .Xdefaults
@@ -31,3 +34,4 @@ echo "XTerm*background: black" >> .Xdefaults
 echo "XTerm*foreground: white" >> .Xdefaults
 
 echo "If it is all ok, run 'startx'"
+
